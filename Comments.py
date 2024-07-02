7b0125add51d57d96933cdb0e32a783b94ab393d
BeginnerPython <furqan6242@gmail.com>
2024-07-02 09:38:52 +0530

    Just Comments

---
+++ Comments.py
@@ -0,0 +1,5 @@
+#This is a comment 
+
+#Anything after the pound symbol is a comment.
+#You can type anything after the pound symbol and the computer wont read it.
+#Normally we use comments to make notes of our programmes.
