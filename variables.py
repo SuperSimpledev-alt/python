e3ba77fa4b6be997b776a983bfa5427ff1baf124
BeginnerPython <furqan6242@gmail.com>
2024-07-02 09:34:23 +0530

    Varibles Added

---
+++ variables.py
@@ -0,0 +1,15 @@
+#variables?
+
+# something where we can store the value in!
+# we need to come up with some cool name for, you know, we cant just use any name.
+
+age = 5 #storing the number 5 into the varibale named age
+
+print(age) # printing the age would give us the result 5.
+
+#The first benifit of variable that it prevents us from changing the code from quadtrillions of places and also reduces the bugs and mistakes.
+
+# Anoter thing we do with variables is we can use them inside of expressions just like you would in normal number
+
+#example
+print(age + 10)
