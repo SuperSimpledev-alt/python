d13b18e1663d53af4ce605bfe468e89eec22cd23
BeginnerPython <furqan6242@gmail.com>
2024-07-02 10:04:14 +0530

    Some Rules

---
+++ naming_varibales.py
@@ -0,0 +1,26 @@
+
+yellow = 5 
+#so here we dont want to make variable name yellow which equals to 5. which does not really make any sense.
+
+#Give your Variable names that means Something so that when you see it you know what it is reffering to.
+
+#Example
+age = 6 #reffering to age. 
+print(age)
+
+age_of_customer = 32 #good
+print(age_of_customer)
+
+#age-3 = 3 #not good.
+#print(age-3)
+
+#4age = 4 #Cannot start a varibale name with a number.
+
+#Age = 1 #This would work but we do not use capital letters in varibales.
+#print(Age)
+
+#return = 5 #can not use Keywords
+#print(return)
+
+#remove the comments one by one and see what results you get. 
+
